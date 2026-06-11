81b13ca23a29368fb35a79314a08bb34f40a5df1
urapotapovich <poyapovich22819@gmail.com>
2026-06-11 15:31:35 +0300

    fix: Убрал лишние данные, которые использовал для тестирования

--- my-taiga-project/src/app/pages/shares/shares.ts
+++ my-taiga-project/src/app/pages/shares/shares.ts
@@ -17,9 +17,5 @@ export class Shares {
   });
 
   protected readonly data = [
-    { name: 'Сбербанк', ticker: 'SBER', price: 312.5, change: 1.2 },
-    { name: 'Газпром', ticker: 'GAZP', price: 164.8, change: -0.8 },
-    { name: 'Лукойл', ticker: 'LKOH', price: 7240.0, change: 2.1 },
-    { name: 'Яндекс', ticker: 'YNDX', price: 4150.0, change: 0.5 },
   ];
 }
