42c5c34b3a78dcb8c09759df485dcd6320e45de1
urapotapovich <poyapovich22819@gmail.com>
2026-06-11 11:47:01 +0300

    feat: Добавлены заглушки выбора языка

--- my-taiga-project/src/app/pages/shares/shares.ts
+++ my-taiga-project/src/app/pages/shares/shares.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import {TuiTitle} from '@taiga-ui/core';
+import {TuiRadio} from '@taiga-ui/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {TuiComboBox} from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-shares',
   imports: [
     TuiTitle
+    , TuiRadio, ReactiveFormsModule, TuiComboBox
   ],
   templateUrl: './shares.html',
   styleUrl: './shares.less',
 })
-export class Shares {}
+export class Shares {
+  form = new FormGroup({
+    choice: new FormControl('option1')
+  });
+}
