f4d568b99acaaab0dfba32f6e9aba958d027db76
urapotapovich <poyapovich22819@gmail.com>
2026-06-16 10:07:47 +0300

    feat: Создана базовая страница с акцией

---
+++ my-taiga-project/src/app/pages/share/share.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-share',
+  imports: [],
+  templateUrl: './share.html',
+  styleUrl: './share.less',
+})
+export class Share {}
