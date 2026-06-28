2399404df780a1f8a14bd3e2479ce4b1522cc590
Vadim Odinokov <njfzvjewhh@gmail.com>
2026-06-28 22:21:08 +0300

    .fix: размещение app-header один раз в app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { TuiRoot } from '@taiga-ui/core';
+import { Header } from './shared/ui/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, TuiRoot],
+  imports: [RouterOutlet, RouterModule, TuiRoot, Header],
   templateUrl: './app.html',
-  styleUrl: './app.less'
+  styleUrl: './app.less',
 })
 export class App {
   protected readonly title = signal('my-taiga-project');
